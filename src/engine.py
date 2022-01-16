04b24e100c86d84b86137304c870a4814a441933
Thomas MICHEL <tmdu13@gmail.com>
2022-01-17 00:04:28 +0100

    Add stochfish engine wrapper

--- src/engine.py
+++ src/engine.py
@@ -3,11 +3,23 @@ import chess
 import chess.pgn
 import chess.engine
 import numpy as np
-import sys
+import os
 from data_generator import encode_board
 import torch
 
-class ChessEngine():
+stream = os.popen("which stockfish")
+
+engine_path = stream.read().split('\n')[0]
+
+
+class ChessEngine:
+    def __init__(self):
+        pass
+    
+    def play(self,board):
+        board.push(board.legal_moves[0])
+
+class TorchEngine(ChessEngine):
     def __init__(self, model, device):
         self.model = model
         self.device = device
@@ -24,3 +36,14 @@ class ChessEngine():
         chosen_move = moves[torch.argmax(scores)]
         return chosen_move
 
+    def play(self, board):
+        board.push(self.get_next_move(board))
+
+class StockfishEngine(ChessEngine):
+    def __init__(self, depth):
+        self.engine = chess.engine.SimpleEngine.popen_uci(engine_path)
+        self.limit = chess.engine.Limit(depth=depth)
+    
+    def play(self,board):
+        self.engine.play(board,self.limit)
+    
