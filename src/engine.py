651815ade04c1ef1a8fa1a1e8e4daa23eb101bdf
Thomas MICHEL <tmdu13@gmail.com>
2022-01-16 23:16:01 +0100

    add basic chess engine based on a torch network

--- src/engine.py
+++ src/engine.py
@@ -1,1 +1,26 @@
 # Transform the model in a chess engine
+import chess
+import chess.pgn
+import chess.engine
+import numpy as np
+import sys
+from data_generator import encode_board
+import torch
+
+class ChessEngine():
+    def __init__(self, model, device):
+        self.model = model
+        self.device = device
+
+    def get_next_move(self, board):
+        moves = board.legal_moves
+        encodings = []
+        for m in moves:
+            board.push(m)
+            encodings.append(encode_board(board))
+            board.pop()
+        encodings = torch.tensor(encodings,device = self.device)
+        scores = self.model(encodings)
+        chosen_move = moves[torch.argmax(scores)]
+        return chosen_move
+
