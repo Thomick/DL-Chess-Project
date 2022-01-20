30d6bbffb1fbe094419af56cb7552535ff7a6f67
Thomas MICHEL <tmdu13@gmail.com>
2022-01-20 23:11:28 +0100

    Add network trainer

--- src/network.py
+++ src/network.py
@@ -20,3 +20,30 @@ class Net(nn.Module):
 
     def forward(self, x):
         return self.layers(x)
+
+
+def train_model(model, dataloader, batch_size, epochs=1):
+    model.train()
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    losses = []
+
+    for epoch in range(epochs):
+        epoch_loss = 0.0
+        for inputs, classes in dataloader:
+            inputs = inputs.to(device)
+            classes = classes.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, classes)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            # statistics
+            epoch_loss += loss.data.item()
+        print(f'Epoch {epoch} Loss: {epoch_loss}')
+        losses.append(epoch_loss)
+
+
+if __name__ == '__main__':
+    model = Net()
