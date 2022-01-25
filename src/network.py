634ea31b2cb8094962a8cf8abd5d49c3204cd392
Thomas MICHEL <tmdu13@gmail.com>
2022-01-25 17:14:50 +0100

    partial training saves and multiple datasets handling

--- src/network.py
+++ src/network.py
@@ -49,16 +49,35 @@ def train_model(model, dataloader, size, epochs=1):
         epoch_loss = running_loss/size
         print(f'Epoch {epoch} Loss: {epoch_loss}')
         losses.append(epoch_loss)
+        if epoch % 10 == 0:
+            np.savetxt(f"../saves/loss{epoch}", np.array(losses))
+            torch.save(model.state_dict(), f"../saves/sd{epoch}")
     return losses
 
+
+def merge_datasets(datasets):
+    finalds = datasets[0]
+    for ds in datasets[1:]:
+        finalds.merge(ds)
+    return finalds
 
 if __name__ == '__main__':
     batch_size = 256
 
+    if len(sys.argv) < 2:
+        print("Please provide datasets")
+        sys.exit()
+
+    datasets_path = sys.argv[1:]
+
+    datasets = []
+    for ds_path in datasets_path:
+        datasets.append(torch.load(ds_path))
+    final_dataset = merge_datasets(datasets)
+
     model = Net()
-    dataset = torch.load("../data/GM_set.pt")
     dataloader = torch.utils.data.DataLoader(
-        dataset, batch_size=batch_size, shuffle=True)
+        final_dataset, batch_size=batch_size, shuffle=True)
     losses = train_model(model, dataloader, len(dataset), epochs=100)
     plt.plot(losses)
     plt.show()
