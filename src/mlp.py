87fcf0253864ed09fac0d8067ff3dfda44f5e6ab
Thomas MICHEL <tmdu13@gmail.com>
2022-01-26 23:48:38 +0100

    Add validation set

--- src/mlp.py
+++ src/mlp.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from data_generator import ChessDataset
-import matplotlib.pyplot as plt
 import sys
 from torch.utils.data import Dataset
 from progressbar import progressbar
@@ -32,24 +31,38 @@ class Net(nn.Module):
         return self.layers(x)
 
 
-def save(model, losses, identifier):
-    np.savetxt(f"../saves/loss{identifier}.txt", np.array(losses))
-    torch.save(model.state_dict(), f"../saves/sd{identifier}.pt")
+def save(model, losses, val_losses, identifier):
+    np.savetxt(f"../saves/mlp/eval_loss{identifier}.txt", np.array(val_losses))
+    np.savetxt(f"../saves/mlp/loss{identifier}.txt", np.array(losses))
+    torch.save(model.state_dict(), f"../saves/mlp/sd{identifier}.pt")
     print("Progress saved !")
 
 
-def train_model(model, dataloader, size, epochs=1):
-    model.train()
+def eval_model(model, val_loader, size):
+    print("Evaluation ...")
+    model.eval()
+    for inputs, target in progressbar(train_loader):
+        inputs = inputs.to(device)
+        targets = torch.unsqueeze(target.to(device).float(), 1)
+        outputs = model(inputs.float())
+        loss = criterion(outputs, targets)
+        running_loss += loss.data.item()
+    return running_loss/size
+
+
+def train_model(model, train_loader, val_loader, size, epochs=1):
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     losses = []
+    val_losses = []
     print(f"Training for {epochs} epochs ...")
 
     for epoch in range(epochs):
+        model.train()
         running_loss = 0.0
         print(f"Starting epoch {epoch} ...")
-        for inputs, target in progressbar(dataloader):
+        for inputs, target in progressbar(train_loader):
             inputs = inputs.to(device)
             targets = torch.unsqueeze(target.to(device).float(), 1)
             outputs = model(inputs.float())
@@ -58,12 +71,16 @@ def train_model(model, dataloader, size, epochs=1):
             loss.backward()
             optimizer.step()
             # statistics
-            running_loss += loss.data.item()/size
-        epoch_loss = running_loss
-        print(f'Epoch {epoch} Loss: {epoch_loss}')
+            running_loss += loss.data.item()
+        epoch_loss = running_loss/size
+        print("Validation")
+        val_loss = eval_model(model, val_loader, size)
+
+        print(f'Epoch {epoch} Loss: {epoch_loss} Validation: {val_loss}')
         losses.append(epoch_loss)
+        val_losses.append(val_loss)
         if epoch % 10 == 0:
-            save(model, losses, epoch)
+            save(model, losses, val_losses, epoch)
     return losses
 
 
@@ -80,6 +97,7 @@ class FixedChessDataset(Dataset):
         sample = (self.encodings[idx][0], self.encodings[idx][1])
         return sample
 
+
 if __name__ == '__main__':
     batch_size = 256
 
@@ -94,11 +112,16 @@ if __name__ == '__main__':
         datasets.append(torch.load(ds_path))
     final_dataset = FixedChessDataset(datasets)
 
+    trainset_size = int(len(final_dataset)*0.8)
+    train_set, val_set = torch.utils.data.random_split(
+        final_dataset, [trainset_size, len(final_dataset)-trainset_size])
+
     print("Size of the dataset : ", len(final_dataset))
     model = Net().to(device)
-    dataloader = torch.utils.data.DataLoader(
-        final_dataset, batch_size=batch_size, shuffle=True)
-    losses = train_model(model, dataloader, len(final_dataset), epochs=1000)
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=batch_size, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(
+        val_set, batch_size=batch_size, shuffle=True)
+    losses, val_losses = train_model(
+        model, train_loader, val_loader, len(final_dataset), epochs=1000)
     save(model, losses, "final")
-    plt.plot(losses)
-    plt.show()
