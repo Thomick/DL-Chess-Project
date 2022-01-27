743b195641a46de1a6d5c249c2b1e5c0bfe425e8
Thomas MICHEL <tmdu13@gmail.com>
2022-01-27 12:20:51 +0100

    Simplify board_to_game

--- src/engine.py
+++ src/engine.py
@@ -18,15 +18,17 @@ engine_path = stream.read().split('\n')[0]
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
 class ChessEngine:
     def __init__(self):
         pass
-    
-    def play(self,board, color):
+
+    def play(self, board, color):
         board.push(random.choice(list(board.legal_moves)))
-    
+
     def quit(self):
         pass
+
 
 class TorchEngine(ChessEngine):
     def __init__(self, model, device):
@@ -41,7 +43,7 @@ class TorchEngine(ChessEngine):
             encodings.append(encode_board(board))
             board.pop()
 
-        encodings = torch.tensor(encodings,device = self.device)
+        encodings = torch.tensor(encodings, device=self.device)
         scores = self.model(encodings) * -1
         chosen_move = moves[torch.argmax(scores)]
         return chosen_move
@@ -61,56 +63,43 @@ class CNN_Engine(ChessEngine):
         encodings = []
         for m in moves:
             board.push(m)
-            encodings.append( mlp_encoding_to_cnn_encoding_board_only(encode_board(board)))
+            encodings.append(
+                mlp_encoding_to_cnn_encoding_board_only(encode_board(board)))
             board.pop()
 
         encodings = np.array(encodings)
         boards = encodings[0]
         metas = encodings[0]
-        boards = torch.tensor(boards,device = self.device)
-        metas = torch.tensor(metas,device = self.device)
-        scores = self.model((boards.float(),metas.float())) * -1
+        boards = torch.tensor(boards, device=self.device)
+        metas = torch.tensor(metas, device=self.device)
+        scores = self.model((boards.float(), metas.float())) * -1
         chosen_move = moves[torch.argmax(scores)]
         return chosen_move
 
     def play(self, board, color):
         board.push(self.get_next_move(board, color))
 
+
 class StockfishEngine(ChessEngine):
     def __init__(self, time, depth):
         self.engine = chess.engine.SimpleEngine.popen_uci(engine_path)
         self.limit = chess.engine.Limit(time=time, depth=depth)
-    
-    def play(self,board, color):
-        result = self.engine.play(board,self.limit)
+
+    def play(self, board, color):
+        result = self.engine.play(board, self.limit)
         board.push(result.move)
 
     def quit(self):
         self.engine.quit()
-    
-def board_to_game(board):
-    game = chess.pgn.Game()
 
-    switchyard = collections.deque()
-    while board.move_stack:
-        switchyard.append(board.pop())
-
-    game.setup(board)
-    node = game
-
-    while switchyard:
-        move = switchyard.pop()
-        node = node.add_variation(move)
-        board.push(move)
-
-    return game
 
 def reset_cursor():
     BEGIN = "\033[F"
     UP = "\033[A"
     print(UP*8 + BEGIN)
 
-def play_game(engine1, engine2, out, max_length = 500):
+
+def play_game(engine1, engine2, out, max_length=500):
     board = chess.Board()
 
     if out:
@@ -130,7 +119,7 @@ def play_game(engine1, engine2, out, max_length = 500):
         if board.is_game_over():
             break
     outcome = board.outcome()
-    print(board_to_game(board))
+    print(chess.pgn.Game().from_board(board))
 
     print(chess.pgn.Game().from_board(board))
     if outcome == None:
@@ -144,7 +133,8 @@ def play_game(engine1, engine2, out, max_length = 500):
     scores = scores.split("-")
     return int(scores[0]), int(scores[1])
 
-def compare_engines(n,engine1,engine2, display):
+
+def compare_engines(n, engine1, engine2, display):
     score1 = 0
     score2 = 0
 
@@ -168,7 +158,7 @@ if __name__ == "__main__":
     stock = StockfishEngine(time=2, depth=12)
     stock2 = StockfishEngine(time=2, depth=11)
     cnn = CNN_Engine(CNN_Net().to(device), device)
-    compare_engines(10,stock, stock2, True)
+    compare_engines(10, stock, stock2, True)
     stock.quit()
     stock2.quit()
     random_engine.quit()
